8d2d85275aa04e2bbd8f6bdc6b2894f7c10f14eb
Jaehyun Kim <seojaeinblank@gmail.com>
2022-02-22 21:50:27 +0900

    feat : Nested PIP

---
+++ android/app/src/main/java/com/superlivecommerce/MainActivity.java
@@ -0,0 +1,22 @@
+package com.superlivecommerce;
+import android.os.Bundle; // 추가!!
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;// 추가!!
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+
+  @Override
+  protected String getMainComponentName() {
+    return "superlivecommerce";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);  // 추가!!
+    super.onCreate(savedInstanceState);
+  }
+}
