9df3a2738112f95b114c9219857182a8b3e6e28a
Jaehyun Kim <seojaeinblank@gmail.com>
2022-03-07 22:53:06 +0900

    add : exoplayer compile setting

--- android/app/src/main/java/com/superlivecommerce/MainApplication.java
+++ android/app/src/main/java/com/superlivecommerce/MainApplication.java
@@ -12,6 +12,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+import com.brentvatne.react.ReactVideoPackage;
+//import java.util.Arrays;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new ReactVideoPackage());
           return packages;
         }
 
